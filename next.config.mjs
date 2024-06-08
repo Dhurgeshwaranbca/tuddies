f65cc25192a3bcc8b45c2ad40a1929600852b5ce
Dhurgeshwaranbca <122508123+Dhurgeshwaranbca@users.noreply.github.com>
2024-06-09 01:34:58 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    images: {
+      loader: 'custom',
+      loaderFile: './src/app/page.tsx',
+    },
+  }
+   
+  module.exports = nextConfig
